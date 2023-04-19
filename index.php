c93253eaa970832f6002685f4810e3911e35d801
Andrii Syzov <new.creator@icloud.com>
2023-04-19 12:39:51 +0300

    Creating a simple site structure

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
-<?php
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <title>MySite.Local</title>
+</head>
+<body>
+<div class="wrapper">
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <span class="fs-4">MySite.Local</span>
+            </a>
 
-    echo "Hello, <b>NIX Education</b>";
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="#" class="nav-link">Урок 1</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Урок 2</a></li>
+            </ul>
+        </header>
+
+        <section class="py-5 text-center container">
+            <div class="row py-lg-5">
+                <div class="col-lg-6 col-md-8 mx-auto">
+                    <h1 class="fw-light">Hello, NIX Education</h1>
+                    <p class="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <footer class="footer mt-auto py-3 bg-light text-center">
+        <div class="container">
+            <span class="text-muted">Fixed site footer</span>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
